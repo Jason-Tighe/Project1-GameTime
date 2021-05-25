ddc778a4e6334bf368769a472f3e62fa83638af5
slowdolphin <tighe59@gmail.com>
2021-05-24 23:20:29 -0400

    I was able to get the sprite to move, but I cant seem to move it

--- js/script.js
+++ js/script.js
@@ -5,42 +5,67 @@ const badBoi = document.querySelector('.badBoi');
 const boop = document.querySelector('#boop')
 const baap = document.querySelector('#baap')
 const dist = document.querySelector('#counter')
-const img = document.querySelector('img')
 
 
-
-canvas.width = 600;
-canvas.height = 600;
+canvas.width = 240;
+canvas.height = 120;
 
 const ctx = canvas.getContext('2d');
 
 
-// const dinoTimer = null
-// const dino ={img:null, x:0, y:0, width:28, height:42, currentframe:0, totalframes:6}
-//
-// dino.img = new Image();
-// dino.img.src = "css/assets/dinobites/DinoSprites - doux.png";
-//
-// dino.img.onload = function(){
-//   dinoTimer = setInterval(animateDino, 120);
-// }
-//
-// function animateDino(){
-//   dino.currentFrame++;
-//   ctx.drawImage(dino.img, dino.currentFrame * dino.width, 0, dino.width, dino.height, 0, 0, dino.width, dino.height);
-//
-//   if(dino.currentFrame >= dino.totalFrame){
-//     dino.currentFrame = 0
-//   }
-// }
+const img = new Image();
+img.src = "css/assets/dinobites/DinoGreen.png";
+
+img.onload = function(){
+  init();
+}
+
+const scale = 1;
+const width = 24;
+const height = 24;
+const scaledWidth = scale * width;
+const scaledHeight = scale * height;
+
+
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, scaledWidth, scaledHeight);
+}
+
+const cycleLoop = [4, 5, 6, 7, 8, 9];
+let currentLoopIndex = 0;
+let frameCount = 0;
+
+function step() {
+  frameCount++;
+  if (frameCount < 9) {
+    window.requestAnimationFrame(step);
+    return;
+  }
+  frameCount = 0;
+  ctx.clearRect(0, 0, width, height);
+  drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
+  currentLoopIndex++;
+  if (currentLoopIndex >= cycleLoop.length) {
+    currentLoopIndex = 0;
+  }
+  window.requestAnimationFrame(step);
+  drawBackgroundLine();
+
+}
+
+function init() {
+  window.requestAnimationFrame(step);
+}
+
 
 
 
 
 function drawBackgroundLine(){
     ctx.beginPath();
-    ctx.moveTo(0,400)
-    ctx.lineTo(600,400)
+    ctx.moveTo(0,80)
+    ctx.lineTo(500,80)
     ctx.lineWidth = 1.9;
     ctx.strokeStyle = "grey"
     ctx.stroke();
@@ -62,103 +87,101 @@ function randomNumberInterval(timeInterval){
   }
   return returnTime
 }
-// const b = canvas.getContext('2d');
-// const g = canvas.getContext('2d');
 
-// g.fillStyle = "darkgrey"
-// g.fillRect(0,775,innerWidth, 1000)
-// g.fillStyle = "grey"
-// g.fillRect(0,775,innerWidth, 25)
-
-class Player{
-    constructor(x, y, size, color){
-    this.x = x;
-    this.y = y;
-    this.size = size;
-    this.color = color;
-    this.jumpHeight = 12;
-    this.shouldJump = false;
-    this.jumpCounter = 0;
-  }
-  jump(){
-    if(this.shouldJump){
-      this.jumpCounter++;
-      if(this.jumpCounter < 15){
-       this.y -= this.jumpHeight;
-    } else if(this.jumpCounter > 14 && this.jumpCounter < 19){
-      this.y += 0;
-    } else if (this.jumpCounter < 33){
-      this.y += this.jumpHeight;
-    }
-
-    if(this.jumpCounter >= 32){
-      this.shouldJump = false;
-    }
-    }
-  }
-  draw(){
-    this.jump();
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.size, this.size);
-  }
-}
-
-
-let player = new Player( 150, 350, 50,"teal");
-
-class Enemy {
-  constructor(size, speed){
-    this.x = canvas.width + size;
-    this.y = 400 - size;
-    this.size = size;
-    this.color = "orange";
-    this.slideSpeed = speed;
-  }
-  draw(){
-    ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, this.size, this.size);
-  }
-  slide(){
-    this.draw();
-    this.x -= this.slideSpeed;
-  }
-}
-
-let pillarMen = []
-
-function generateBaddies (){
-  let timeDelay = randomNumberInterval(preSetTime);
-  pillarMen.push(new Enemy(45, enemSpeed))
-
-  setTimeout(generateBaddies, timeDelay)
-}
-
-
-setTimeout(()=>{
-  generateBaddies();
-},randomNumberInterval(preSetTime))
+//
+// class Player{
+//     constructor(x, y, size, color){
+//     this.x = x;
+//     this.y = y;
+//     this.size = size;
+//     this.color = color;
+//     this.jumpHeight = 12;
+//     this.shouldJump = false;
+//     this.jumpCounter = 0;
+//   }
+//   jump(){
+//     if(this.shouldJump){
+//       this.jumpCounter++;
+//       if(this.jumpCounter < 15){
+//        this.y -= this.jumpHeight;
+//     } else if(this.jumpCounter > 14 && this.jumpCounter < 19){
+//       this.y += 0;
+//     } else if (this.jumpCounter < 33){
+//       this.y += this.jumpHeight;
+//     }
+//
+//     if(this.jumpCounter >= 32){
+//       this.shouldJump = false;
+//     }
+//     }
+//   }
+//   draw(){
+//     this.jump();
+//     ctx.fillStyle = this.color;
+//     ctx.fillRect(this.x, this.y, this.size, this.size);
+//   }
+// }
+//
+//
+// let player = new Player( 150, 350, 50,"teal");
+//
+// class Enemy {
+//   constructor(size, speed){
+//     this.x = canvas.width + size;
+//     this.y = 400 - size;
+//     this.size = size;
+//     this.color = "orange";
+//     this.slideSpeed = speed;
+//   }
+//   draw(){
+//     ctx.fillStyle = this.color;
+//     ctx.fillRect(this.x, this.y, this.size, this.size);
+//   }
+//   slide(){
+//     this.draw();
+//     this.x -= this.slideSpeed;
+//   }
+// }
 
 
 
-function animate(){
-  requestAnimationFrame(animate);
-  ctx.clearRect(0,0,canvas.width, canvas.height)
-
+//
+// let pillarMen = []
+//
+//
+//
+// function generateBaddies (){
+//   let timeDelay = randomNumberInterval(preSetTime);
+//   pillarMen.push(new Enemy(45, enemSpeed))
+//   setTimeout(generateBaddies, timeDelay)
+// }
+//
+//
+// setTimeout(()=>{
+//   generateBaddies();
+//  },randomNumberInterval(preSetTime))
+//
+//
+//
+// function animate(){
+//   requestAnimationFrame(animate);
+//   ctx.clearRect(0,0,canvas.width, canvas.height)
+//
   drawBackgroundLine();
-
-  player.draw();
-
-  pillarMen.forEach((pillarMan, index) => {
-    pillarMan.slide();
-    if((pillarMan.x + pillarMan.size) <= 0){
-      setTimeout(() => {
-        pillarMen.splice(index, 1);
-      }, 0)
-    }
-  });
-}
-
-animate();
+//
+//   player.draw();
+//
+//   pillarMen.forEach((pillarMan, index) => {
+//     pillarMan.slide();
+//     if((pillarMan.x + pillarMan.size) <= 0){
+//       setTimeout(() => {
+//         pillarMen.splice(index, 1);
+//       }, 0)
+//     }
+//   });
+// }
+//
+// animate();
 
 
 
