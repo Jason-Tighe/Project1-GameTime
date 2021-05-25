30ff0aba6b8d2313be1dfbc5aec54a41e7c06e65
slowdolphin <tighe59@gmail.com>
2021-05-24 23:38:33 -0400

    brought back the regular blocks and messed with jmp key

--- js/script.js
+++ js/script.js
@@ -7,8 +7,8 @@ const baap = document.querySelector('#baap')
 const dist = document.querySelector('#counter')
 
 
-canvas.width = 240;
-canvas.height = 120;
+canvas.width = 600;
+canvas.height = 600;
 
 const ctx = canvas.getContext('2d');
 
@@ -51,7 +51,6 @@ function step() {
   }
   window.requestAnimationFrame(step);
   drawBackgroundLine();
-
 }
 
 function init() {
@@ -61,11 +60,10 @@ function init() {
 
 
 
-
 function drawBackgroundLine(){
     ctx.beginPath();
-    ctx.moveTo(0,80)
-    ctx.lineTo(500,80)
+    ctx.moveTo(0,400)
+    ctx.lineTo(600,400)
     ctx.lineWidth = 1.9;
     ctx.strokeStyle = "grey"
     ctx.stroke();
@@ -88,100 +86,100 @@ function randomNumberInterval(timeInterval){
   return returnTime
 }
 
-//
-// class Player{
-//     constructor(x, y, size, color){
-//     this.x = x;
-//     this.y = y;
-//     this.size = size;
-//     this.color = color;
-//     this.jumpHeight = 12;
-//     this.shouldJump = false;
-//     this.jumpCounter = 0;
-//   }
-//   jump(){
-//     if(this.shouldJump){
-//       this.jumpCounter++;
-//       if(this.jumpCounter < 15){
-//        this.y -= this.jumpHeight;
-//     } else if(this.jumpCounter > 14 && this.jumpCounter < 19){
-//       this.y += 0;
-//     } else if (this.jumpCounter < 33){
-//       this.y += this.jumpHeight;
-//     }
-//
-//     if(this.jumpCounter >= 32){
-//       this.shouldJump = false;
-//     }
-//     }
-//   }
-//   draw(){
-//     this.jump();
-//     ctx.fillStyle = this.color;
-//     ctx.fillRect(this.x, this.y, this.size, this.size);
-//   }
-// }
-//
-//
-// let player = new Player( 150, 350, 50,"teal");
-//
-// class Enemy {
-//   constructor(size, speed){
-//     this.x = canvas.width + size;
-//     this.y = 400 - size;
-//     this.size = size;
-//     this.color = "orange";
-//     this.slideSpeed = speed;
-//   }
-//   draw(){
-//     ctx.fillStyle = this.color;
-//     ctx.fillRect(this.x, this.y, this.size, this.size);
-//   }
-//   slide(){
-//     this.draw();
-//     this.x -= this.slideSpeed;
-//   }
-// }
+
+class Player{
+    constructor(x, y, size, color){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    this.jumpHeight = 12;
+    this.shouldJump = false;
+    this.jumpCounter = 0;
+  }
+  jump(){
+    if(this.shouldJump){
+      this.jumpCounter++;
+      if(this.jumpCounter < 15){
+       this.y -= this.jumpHeight;
+    } else if(this.jumpCounter > 14 && this.jumpCounter < 19){
+      this.y += 0;
+    } else if (this.jumpCounter < 33){
+      this.y += this.jumpHeight;
+    }
+
+    if(this.jumpCounter >= 32){
+      this.shouldJump = false;
+    }
+    }
+  }
+  draw(){
+    this.jump();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  }
+}
+
+
+let player = new Player( 150, 350, 50,"teal");
+
+class Enemy {
+  constructor(size, speed){
+    this.x = canvas.width + size;
+    this.y = 400 - size;
+    this.size = size;
+    this.color = "orange";
+    this.slideSpeed = speed;
+  }
+  draw(){
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  }
+  slide(){
+    this.draw();
+    this.x -= this.slideSpeed;
+  }
+}
 
 
 
-//
-// let pillarMen = []
-//
-//
-//
-// function generateBaddies (){
-//   let timeDelay = randomNumberInterval(preSetTime);
-//   pillarMen.push(new Enemy(45, enemSpeed))
-//   setTimeout(generateBaddies, timeDelay)
-// }
-//
-//
-// setTimeout(()=>{
-//   generateBaddies();
-//  },randomNumberInterval(preSetTime))
-//
-//
-//
-// function animate(){
-//   requestAnimationFrame(animate);
-//   ctx.clearRect(0,0,canvas.width, canvas.height)
-//
+
+let pillarMen = []
+
+
+
+function generateBaddies (){
+  let timeDelay = randomNumberInterval(preSetTime);
+  pillarMen.push(new Enemy(45, enemSpeed))
+  setTimeout(generateBaddies, timeDelay)
+}
+
+
+setTimeout(()=>{
+  generateBaddies();
+ },randomNumberInterval(preSetTime))
+
+
+
+function animate(){
+  requestAnimationFrame(animate);
+  ctx.clearRect(0,0,canvas.width, canvas.height)
+
   drawBackgroundLine();
-//
-//   player.draw();
-//
-//   pillarMen.forEach((pillarMan, index) => {
-//     pillarMan.slide();
-//     if((pillarMan.x + pillarMan.size) <= 0){
-//       setTimeout(() => {
-//         pillarMen.splice(index, 1);
-//       }, 0)
-//     }
-//   });
-// }
-//
-// animate();
+
+  player.draw();
+
+  pillarMen.forEach((pillarMan, index) => {
+    pillarMan.slide();
+    if((pillarMan.x + pillarMan.size) <= 0){
+      setTimeout(() => {
+        pillarMen.splice(index, 1);
+      }, 0)
+    }
+  });
+}
+
+animate();
 
 
 
@@ -190,7 +188,7 @@ function randomNumberInterval(timeInterval){
 
 
 addEventListener("keydown", (e)=>{
-  if(e.code === 'Space'){
+  if(event.key === 'Shift'){
     if(!player.shouldJump){
       player.jumpCounter=0;
       player.shouldJump = true;
