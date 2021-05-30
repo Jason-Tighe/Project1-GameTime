9b526274c4143055faf4f58f640a3e930973aff1
slowdolphin <tighe59@gmail.com>
2021-05-30 17:52:30 -0400

    in the end i never figured out how to combine all the parts.

--- js/script.js
+++ js/script.js
@@ -1,152 +1,54 @@
-const game = document.querySelector('.game')
-// const canvas = document.querySelector('canvas')
-const mainBoi = document.querySelector('.mainBoi');
-const badBoi = document.querySelector('.badBoi');
-const boop = document.querySelector('#boop')
-const baap = document.querySelector('#baap')
-const dist = document.querySelector('#counter')
+const canvas = document.querySelector('canvas')
+const dist = document.querySelector('.distance')
 
 
 //this function adds and removes the animate class to the player tag "mainBoi". This allows us to jump using css, but also takes it away so we can reapply that animation.
 
-function jump(){
-  if(mainBoi.classList == "animate"){return}
-  mainBoi.classList.add("animate");
-  setTimeout(function(){
-    mainBoi.classList.remove("animate")
-  },600)
-}
-
-
-let x = mainBoi.offSetTop;
-console.log(x)
+// function jump(){
+//   if(mainBoi.classList == "animate"){return}
+//   mainBoi.classList.add("animate");
+//   setTimeout(function(){
+//     mainBoi.classList.remove("animate")
+//   },600)
+// }
+//
+//
+// let x = mainBoi.offSetTop;
+// console.log(x)
 
 
 //this is a rough collision function, it checks to see if the character Mainboi;s top touches the left of the enemy block.
 //
+//
+// function buttBump(){
+//   let mainBoiTop = parseInt(window.getComputedStyle(mainBoi).getPropertyValue("top"));
+//   let badBoiLeft = parseInt(window.getComputedStyle(badBoi).getPropertyValue("left"));
+//   if(badBoiLeft < 48 && badBoiLeft > -48 && mainBoiTop >= 200){
+//     badBoi.style.animation ="none";
+//     alert("game over boi")
+//   }
+// }
 
-function buttBump(){
-  let mainBoiTop = parseInt(window.getComputedStyle(mainBoi).getPropertyValue("top"));
-  let badBoiLeft = parseInt(window.getComputedStyle(badBoi).getPropertyValue("left"));
-  if(badBoiLeft < 50 && badBoiLeft > -50 && mainBoiTop >= 200){
-    badBoi.style.animation ="none";
-    alert("game over boi")
-    // badBoi.style.animation ="block 1s infinite linear";
-  }
-}
-
-
-const deadYet = setInterval((e)=> buttBump(), 10);
-
-console.log(deadYet)
-
-addEventListener("click", (e)=>{jump()});
+//
+// const deadYet = setInterval((e)=> buttBump(), 10);
+//
+// console.log(deadYet)
+//
+// addEventListener("click", (e)=>{jump()});
 
 // found this tip on resizing the canvas
 // window.addEventListener('resize', function(){
 //     canvas.height = window.innerHeight;
 //     canvas.width = window.innerWidth;
 // })
-// canvas.width = 600;
-// canvas.height = 300;
-//
-// const ctx = canvas.getContext('2d');
-//
-// function background() {
-//   ctx.fillStyle="blue"
-//   ctx.fillRect=(0,0,600,3)
-//
-// }
-// background()
-//
-// img = new Image();
-// img.src = "css/assets/dinobites/DinoRed.png";
-//
-// img.onload = function(){
-//   init();
-// }
+canvas.width = 600;
+canvas.height = 600;
 
-//I'm able to make the class for the player and draw the image. I'm having trouble animating it.
-//
-// class Dino{
-//   constructor(x, y){
-//     this.scale = 4;
-//     this.x = x;
-//     this.y = y;
-//     this.width = 24 ;
-//     this.height = 24 ;
-//     this.canvasX = 10;
-//     this.canvasY = canvas.height- 100;
-//     this.scaledWidth = this.scale * this.width;
-//     this.scaledHeight = this.scale * this.height;
-//     this.cycleLoop = [4, 5, 6, 7, 8, 9];
-//     this.currentLoopIndex = 0;
-//     this.frameCount = 0;
-//     this.image = 'css/assets/dinobites/DinoGreen.png'
-//   }
-//   drawImage() {
-//       var image = new Image();
-//       image.onload = () => {
-//         ctx.mozImageSmoothingEnabled = false;
-//         ctx.imageSmoothingEnabled = false;
-//         ctx.drawImage(image, this.x * this.width, this.y * this.height, this.width, this.height, this.canvasX, this.canvasY, this.scaledWidth, this.scaledHeight);
-//       };
-//       image.src = this.image;
-//     }
-// }
+const ctx = canvas.getContext('2d');
 
 
-//attempt to pust objects into an array and animate through that. Did not knwo how to clear previous drawings
-// const loop = [new Dino(4, 0), new Dino(5, 0), new Dino(6, 0), new Dino(7, 0), new Dino(8, 0), new Dino(9, 0)]
-// function animate(){
-// for(let i = 0; i < loop.length; i++){
-//   if(i > loop.length){
-//     let i = 0;
-//   }
-//   const green = loop[i]
-//   green.drawImage()
-//   loop.splice(0,1)
-//   }
-// }
 
 
-// const green = new Dino(4,0)
-// green.drawImage()
-//
-// console.log(green.animate)
-//
-// console.log(green.step)
-// console.log(Dino)
-// console.log(green.drawImage)
-
-
-//
-// const images = {};
-// images.sprite = new Image();
-// images.sprite.src = 'css/assets/dinobites/DinoGreen.png'
-
-
-// step() {
-//  const cycleLoop = [4, 5, 6, 7, 8, 9];
-//  let currentLoopIndex = 0;
-//  let frameCount = 0;
-//  frameCount++;
-//  if (frameCount < 1) {
-//    window.requestAnimationFrame();
-//    return;
-//  }
-//  frameCount = 0;
-//  ctx.clearRect(0, 0, width, height);
-//  drawFrame(cycleLoop[currentLoopIndex], 0, 0, 0);
-//  currentLoopIndex++;
-//  if (currentLoopIndex >= cycleLoop.length) {
-//    currentLoopIndex = 0;
-//  }
-//  window.requestAnimationFrame(step);
-// }
-// init() {
-//  window.requestAnimationFrame(step);
-// }
 
 
 //this is an animated dino boy, I can turn these const to part of a class and the functions to methods. BUT I need to move this dude first.
@@ -161,210 +63,192 @@ addEventListener("click", (e)=>{jump()});
 // const cycleLoop = [4, 5, 6, 7, 8, 9];
 // let currentLoopIndex = 0;
 // let frameCount = 0;
+// let img = new Image();
 //
 // function drawFrame(frameX, frameY, canvasX, canvasY) {
-//   ctx.drawImage(img, frameX * width, frameY * height, width, height, canvasX, canvasY, scaledWidth, scaledHeight);
+//   ctx.drawImage(img.src ="css/assets/dinobites/DinoRed.png", frameX * width, frameY * height, width, height, canvasX, canvasY, scaledWidth, scaledHeight);
 //
 //   ctx.mozImageSmoothingEnabled =false;
 //   ctx.imageSmoothingEnabled = false;
 //
 // }
 //
-//
-// function step() {
-//   frameCount++;
-//   if (frameCount < 6) {
-//     window.requestAnimationFrame(step);
-//     return;
-//   }
-//   frameCount = 0;
-//   ctx.clearRect(canvasX, canvasY, scale * width, scale * height);
-//   drawFrame(cycleLoop[currentLoopIndex], 0, canvasX, canvasY);
-//   currentLoopIndex++;
-//   if (currentLoopIndex >= cycleLoop.length) {
-//     currentLoopIndex = 0;
-//   }
-//   window.requestAnimationFrame(step);
-//
-// }
-//
-// function init() {
-//   window.requestAnimationFrame(step);
-// }
-// function bang(){
-//   ctx.clearRect(canvasX, canvasY, scale * width, scale * height)
-// }
-//
-// function jump(){
-//   canvasY -= 60
-// }
-//
-// function land(){
-//   if(canvasY < 200){
-//   canvasY += 25
-//  }
-// }
-//
-// function softLand(){
-//   while(canvasY < 200){
-//   }  setInterval(land(),600)
-// }
-//
-// //janky jump button
-// addEventListener("keydown", (e)=>{
-//   if(event.key === 'Shift'){
-//
-//       bang();
-//       jump();
-//     setInterval(()=> land(), 300, ()=> bang())
-//   }
-// })
-
-//From this point down game is almost done.
-//
-// function drawBackgroundLine(){
-//     ctx.beginPath();
-//     ctx.moveTo(0,400)
-//     ctx.lineTo(600,400)
-//     ctx.lineWidth = 1.9;
-//     ctx.strokeStyle = "grey"
-//     ctx.stroke();
-// }
-//
-//
-// drawBackgroundLine();
-//
-// let preSetTime = 1000;
-// let enemSpeed = 6 ;
-//
-// function getRandomNumber(min, max){
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-//
-// function randomNumberInterval(timeInterval){
-//   let returnTime = timeInterval;
-//   if(Math.random() < .5){
-//     returnTime =+ getRandomNumber(preSetTime%3, preSetTime * 1.5);
-//   } else {
-//     returnTime -= getRandomNumber(preSetTime%5, preSetTime%2);
-//   }
-//   return returnTime
-// }
-//
-//
-// class Player{
-//     constructor(x, y, size, color){
-//     this.x = x;
-//     this.y = y;
-//     this.size = size;
-//     this.color = color;
-//     this.jumpHeight = 12;
-//     this.shouldJump = false;
-//     this.jumpCounter = 0;
-//   }
-//   jump(){
-//     if(this.shouldJump){
-//       this.jumpCounter++;
-//       if(this.jumpCounter < 15){
-//        this.y -= this.jumpHeight;
-//     } else if(this.jumpCounter > 14 && this.jumpCounter < 19){
-//       this.y += 0;
-//     } else if (this.jumpCounter < 33){
-//       this.y += this.jumpHeight;
-//     }
-//
-//     if(this.jumpCounter >= 32){
-//       this.shouldJump = false;
-//     }
-//     }
-//   }
-//   draw(){
-//     this.jump();
-//
-//     ctx.fillStyle = this.color;
-//     ctx.fillRect(this.x, this.y, this.size, this.size);
-//   }
-//   collide(){
-//     }
-// }
-//
-// let player = new Player( 150, 350, 50,"teal");
-
-//
-// class Enemy {
-//   constructor(size, speed){
-//     this.x = canvas.width + size;
-//     this.y = 400 - size;
-//     this.size = size;
-//     this.color = "orange";
-//     this.slideSpeed = speed;
-//   }
-//   draw(){
-//     ctx.fillStyle = this.color;
-//     ctx.fillRect(this.x, this.y, this.size, this.size);
-//   }
-//   slide(){
-//     this.draw();
-//     this.x -= this.slideSpeed;
-//   }
-// }
-//
-//
-//
-//
-// let pillarMen = []
-//
-//
-//
-// function generateBaddies (){
-//   let timeDelay = randomNumberInterval(preSetTime);
-//   pillarMen.push(new Enemy(45, enemSpeed))
-//   setTimeout(generateBaddies, timeDelay)
-// }
-//
-//
-// setTimeout(()=>{
-//   generateBaddies();
-//  },randomNumberInterval(preSetTime))
 
 
 
-// function animate(){
-//   requestAnimationFrame(animate);
-//   ctx.clearRect(0,0,canvas.width, canvas.height)
-//
-//   drawBackgroundLine();
-//
-//   player.draw();
-//
-//   pillarMen.forEach((pillarMan, index) => {
-//     pillarMan.slide();
-//     if((pillarMan.x + pillarMan.size) <= 0){
-//       setTimeout(() => {
-//         pillarMen.splice(index, 1);
-//       }, 0)
-//     }
-//   });
-// }
-//
-// animate();
+// From this point down game is almost done.
+
+function drawBackgroundLine(){
+    ctx.beginPath();
+    ctx.moveTo(0,400)
+    ctx.lineTo(600,400)
+    ctx.lineWidth = 1.9;
+    ctx.strokeStyle = "grey"
+    ctx.stroke();
+}
+
+
+drawBackgroundLine();
+
+let preSetTime = 2000;
+let enemSpeed = 8 ;
+
+function getRandomNumber(min, max){
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomNumberInterval(timeInterval){
+  let returnTime = timeInterval;
+  if(Math.random() < .5){
+    returnTime =+ getRandomNumber(preSetTime%3, preSetTime * 1.5);
+  } else {
+    returnTime -= getRandomNumber(preSetTime%5, preSetTime%2);
+  }
+  return returnTime
+}
+
+
+//this is the collision function, it's checking to see if the
+
+function squaresColliding(player,block){
+    let s1 = Object.assign(Object.create(Object.getPrototypeOf(player)), player);
+    let s2 = Object.assign(Object.create(Object.getPrototypeOf(block)), block);
+    s2.size = s2.size;
+    s2.x = s2.x + 10;
+    s2.y = s2.y + 10;
+    return !(
+        s1.x>s2.x+s2.size || //R1 is to the right of R2
+        s1.x+s1.size<s2.x || //R1 to the left of R2
+        s1.y>s2.y+s2.size || //R1 is below R2
+        s1.y+s1.size<s2.y //R1 is above R2\
+    )
+}
+
+
+//player class
+class Player{
+    constructor(x, y, size, color){
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.color = color;
+    this.jumpHeight = 12;
+    this.shouldJump = false;
+    this.jumpCounter = 0;
+  }
+
+  // this is the jump method it works on a 32 frame sequence, basically once it reaches 32frames,
+  // which would be the top of the jump, it clears it back down to 0.
+  // this also prevents the player from jumping more than once,
+
+  jump(){
+    if(this.shouldJump){
+      this.jumpCounter++;
+      if(this.jumpCounter < 15){
+       this.y -= this.jumpHeight;
+    } else if(this.jumpCounter > 14 && this.jumpCounter < 19){
+      this.y += 0;
+    } else if (this.jumpCounter < 33){
+      this.y += this.jumpHeight;
+    }
+
+    if(this.jumpCounter >= 32){
+      this.shouldJump = false;
+    }
+    }
+  }
+  draw(){
+    this.jump();
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  }
+}
+
+
+// new player
+let player = new Player( 150, 350, 50,"teal");
+
+//enemy class
+class Enemy {
+  constructor(size, speed){
+    this.x = canvas.width + size;
+    this.y = 400 - size;
+    this.size = size;
+    this.color = "orange";
+    this.slideSpeed = speed;
+  }
+  draw(){
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.size, this.size);
+  }
+  slide(){
+    this.draw();
+    this.x -= this.slideSpeed;
+  }
+}
 
 
 
+//enemy array
+let blocks = []
+
+
+//this creates more enemies and pushes them into the array
+
+function generateBaddies (){
+  let timeDelay = randomNumberInterval(preSetTime);
+  blocks.push(new Enemy(45, enemSpeed))
+  setTimeout(generateBaddies, timeDelay)
+}
 
 
 
-//
-// addEventListener("keydown", (e)=>{
-//   if(event.key === 'Shift'){
-//     if(!player.shouldJump){
-//       player.jumpCounter=0;
-//       player.shouldJump = true;
-//     }
-//   }
-// })
+//this generates new enemies at a random interval
+setTimeout(()=>{
+  generateBaddies();
+ },randomNumberInterval(preSetTime))
 
-//
+
+//this is the animation function, this draws the player, the base line, consitently clears them aswell
+//this also animates the sliding of the enemies, checks for collision, and deletes enemies out of the array
+let animationId = null;
+function animate(){
+  animationId = requestAnimationFrame(animate);
+  ctx.clearRect(0,0,canvas.width, canvas.height)
+
+  drawBackgroundLine();
+
+  player.draw();
+
+  blocks.forEach((block, index) => {
+    block.slide();
+
+    if(squaresColliding(player, block)){
+      cancelAnimationFrame(animationId);
+    }
+
+    if((block.x + block.size) <= 0){
+      setTimeout(() => {
+        blocks.splice(index, 1);
+      }, 0)
+    }
+  });
+}
+
+animate();
+
+
+//this is the button to make the player jump.
+addEventListener("keydown", (e)=>{
+  if(event.key === 'Shift'){
+    if(!player.shouldJump){
+      player.jumpCounter=0;
+      player.shouldJump = true;
+    }
+  }
+})
+
+
 // function county() {
 //   let x = 0
 //   let max = 10;
